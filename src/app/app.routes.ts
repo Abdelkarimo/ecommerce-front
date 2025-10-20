33c84754c332006a446a2a765a6cf2b3951f0dd0
Barghout22 <barghoutmahmoud2@gmail.com>
2025-10-20 22:49:08 +0300

    merge changes with the main branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { CartComponent } from './features/cart/cart/cart';
-// import { Landing } from './features/landing/landing/landing';
-// import { FavouriteComponent } from './features/favourites/favourite';
-// import { CheckoutComponent } from './features/cart/checkout/checkout';
-// import { OrderConfirmation } from './features/order-confirmation/order-confirmation';
-
-// export const routes: Routes = [
-//     { path: '', redirectTo: 'landing', pathMatch: 'full' },
-//     { path: 'landing', component: Landing, title: 'Landing Page' },
-//     { path: 'cart', component: CartComponent, title: 'Cart' },
-//         { path: 'favorites', component: FavouriteComponent, title: 'Favorites' },
-//         // compatibility: some templates use 'favourites' (british spelling)
-//         { path: 'favourites', redirectTo: 'favorites' },
-//     { path: 'checkout', component: CheckoutComponent, title: 'Checkout' },
-//     { path: 'order-confirmation', component: OrderConfirmation, title: 'Order Confirmation' },
-//     // fallback
-//     { path: '**', redirectTo: 'landing' }
-// ];
-
 import { Routes } from '@angular/router';
 
 // Layouts
@@ -40,7 +20,6 @@ import { FavouriteComponent } from './features/favourites/favourite';
 // Admin
 import { AdminDashboard } from './features/admin/admin-dashboard/admin-dashboard';
 import { ProductCrud } from './features/admin/product-crud/product-crud';
-import { authGuard } from './core/auth/auth-guard';
 import { authGuard } from './core/auth/auth-guard';
 
 export const routes: Routes = [
