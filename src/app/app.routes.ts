a9ff9af402c64d8046c2c58209c1d4588b66518c
Ahmedhatem307 <84460531+Ahmedhatem307@users.noreply.github.com>
2025-10-20 21:21:32 +0300

    save app route edits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
-import { Routes } from '@angular/router';
 import { ProductList } from './features/products/product-list/product-list';
 import { Landing } from './features/landing/landing/landing';
 import { About } from './features/about/about/about';
 import { FavouriteList } from './features/favourites/favourite-list/favourite-list';
 import { CategoryList } from './features/category-list/category-list';
 import { ProductDetail } from './features/products/product-detail/product-detail';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: Landing },
